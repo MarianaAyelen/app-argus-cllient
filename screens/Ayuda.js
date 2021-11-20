f8ace7423a29e3fb8520796cb497c4655fb33869
Mariana Romero <maromerov@fi.uba.ar>
2021-11-20 01:11:16 -0300

    add help

--- screens/Ayuda.js
+++ screens/Ayuda.js
@@ -9,11 +9,18 @@ export default function Ayuda(){
             <StatusBar style="auto" />
             <HeaderApp />
             <Text style={styles.nosotros}>Nosotros</Text>
-            <Text style={styles.basicText}>Argus es una aplicación que permite ...</Text>
+            <Text style={styles.basicText}>Argus es una aplicación que permite cuidar a tus seres queridos, objetos de valor o lo que desees</Text>
             <Text style={styles.nosotros}>Configuración</Text>
-            <Text style={styles.basicText}>Para configurar el módulo...</Text>
-            <Text style={styles.enums}>1-... </Text>
-            <Text style={styles.enums}>2-... </Text>
+            <Text style={styles.basicText}>Para configurar el módulo debes seguir los siguientes pasos:</Text>
+            <Text style={styles.enums}>1-   Entrar a la sección "Configurar módulo" y cargar todos los datos. Tené a mano el número de serie! </Text>
+            <Text style={styles.enums}>2-   Cuando el módulo encienda la luz, precionar el botón verde. Listo! Ya podés monitorear lo que desees! </Text>
+            <Text style={styles.nosotros}>Funcionalidades:</Text>
+            <Text style={styles.enums}>1-  Ver la ubicación en tiempo real en la sección "Ver mapa"</Text>
+            <Text style={styles.enums}>2-  Configurar zona segura y zona peligrosa en la sección "Zona segura/peligrosa". Recibirás una alerta si no se cumplen!</Text>
+            <Text style={styles.enums}>3-  Ver las últimas actividades del módulo en la sección "Ver actividad"</Text>
+            <Text style={styles.enums}>4-  Comunicarte con el módulo en la sección "Llamar"</Text>
+            <Text style={styles.names}>By Jazmin Ferreyra & Mariana Romero</Text>
+            
         </View>
     );
 }
@@ -31,6 +38,7 @@ const styles = StyleSheet.create({
         fontSize: 15,
         marginTop: 1,
         marginLeft: 15,
+        marginRight: 5
       },
       enums: {
         fontFamily: 'serif',
@@ -38,6 +46,7 @@ const styles = StyleSheet.create({
         fontSize: 15,
         marginTop: 1,
         marginLeft: 40,
+        marginRight: 5
       },
       nosotros: {
         fontFamily: 'serif',
@@ -45,5 +54,12 @@ const styles = StyleSheet.create({
         fontSize: 20,
         marginTop: 20,
         marginLeft: 15,
+      },
+      names: {
+        fontFamily: 'serif',
+        color: '#2E86C1',
+        fontSize: 12,
+        marginTop: 50,
+        marginLeft: 150,
       }
 });
